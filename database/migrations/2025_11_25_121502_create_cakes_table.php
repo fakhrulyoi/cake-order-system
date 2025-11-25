f8b340eda9793646f30c1fa41a48d43be0fd79ef
fakhrulyoi <yoifakrul@gmail.com>
2025-11-25 22:08:25 +0800

    `Updated migration files to conform to new migration syntax`

--- database/migrations/2025_11_25_121502_create_cakes_table.php
+++ database/migrations/2025_11_25_121502_create_cakes_table.php
@@ -1,14 +1,28 @@
-public function up()
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
 {
-    Schema::create('cakes', function (Blueprint $table) {
-        $table->id();
-        $table->string('name');
-        $table->text('description')->nullable();
-        $table->decimal('price', 8, 2);
-        $table->string('size')->nullable();
-        $table->string('image')->nullable();
-        $table->enum('status', ['available', 'unavailable'])->default('available');
-        $table->string('category')->nullable();
-        $table->timestamps();
-    });
-}
+    public function up()
+    {
+        Schema::create('cakes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price', 8, 2);
+            $table->string('size')->nullable();
+            $table->string('image')->nullable();
+            $table->enum('status', ['available', 'unavailable'])->default('available');
+            $table->string('category')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('cakes');
+    }
+};
